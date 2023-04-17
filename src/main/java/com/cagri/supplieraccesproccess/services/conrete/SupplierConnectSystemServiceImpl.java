9a7af5c3212d647c1378a3baa1f6b934290cbcc9
Çağrı KART <ckarte43@gmail.com>
2023-04-17 14:10:08 +0300

    person, scs,supplier ekle ve list bitti

--- src/main/java/com/cagri/supplieraccesproccess/services/conrete/SupplierConnectSystemServiceImpl.java
+++ src/main/java/com/cagri/supplieraccesproccess/services/conrete/SupplierConnectSystemServiceImpl.java
@@ -3,6 +3,7 @@ package com.cagri.supplieraccesproccess.services.conrete;
 import com.cagri.supplieraccesproccess.core.utilities.results.DataResult;
 import com.cagri.supplieraccesproccess.core.utilities.results.Result;
 import com.cagri.supplieraccesproccess.core.utilities.results.SuccessDataResult;
+import com.cagri.supplieraccesproccess.core.utilities.results.SuccessResult;
 import com.cagri.supplieraccesproccess.dto.SupplierConnectSystemRequest;
 import com.cagri.supplieraccesproccess.entities.SupplierConnectSystem;
 import com.cagri.supplieraccesproccess.repository.SupplierConnectSystemRepository;
@@ -29,11 +30,23 @@ public class SupplierConnectSystemServiceImpl implements SupplierConnectSystemSe
         return new SuccessDataResult<List<SupplierConnectSystem>>(supplierConnectSystemRepository.findAll(),"listed all scs ");
     }
 
-    @Override
-    public Result addScS(List<SupplierConnectSystemRequest> supplierConnectSystemRequests) {
+//    @Override
+//    public Result addScS(List<SupplierConnectSystemRequest> supplierConnectSystemRequests) {
+//        List<SupplierConnectSystem> supplierConnectSystems = supplierConnectSystemRequests.stream()
+//                .map(SupplierConnectSystemServiceImpl::mapToScs)
+//                .collect(Collectors.toList());
+//        this.supplierConnectSystemRepository.save();
+//        return  new SuccessResult("added");
+//    }
+
+    public DataResult<List<SupplierConnectSystem>> addScS(List<SupplierConnectSystemRequest> supplierConnectSystemRequests) {
         List<SupplierConnectSystem> supplierConnectSystems = supplierConnectSystemRequests.stream()
                 .map(SupplierConnectSystemServiceImpl::mapToScs)
                 .collect(Collectors.toList());
-        return null;
+
+        List<SupplierConnectSystem> savedSystems = supplierConnectSystemRepository.saveAll(supplierConnectSystems);
+
+
+        return new SuccessDataResult<List<SupplierConnectSystem>>(savedSystems,"SupplierConnectSystem objects added successfully");
     }
 }
