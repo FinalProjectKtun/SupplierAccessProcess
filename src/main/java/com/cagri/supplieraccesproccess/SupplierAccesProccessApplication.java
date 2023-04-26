948a8ec5c3a12f43c27f85accc9052a0fbb7c0a5
abdullahokay <107299543+abdullahokay@users.noreply.github.com>
2023-04-26 23:49:23 +0300

    changed main class name

--- src/main/java/com/cagri/supplieraccesproccess/SupplierAccesProccessApplication.java
+++ src/main/java/com/cagri/supplieraccesproccess/SupplierAccesProccessApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SupplierAccesProccessApplication {
+public class SupplierAccessProccessApplication {
 
     public static void main(String[] args) {
-            SpringApplication.run(SupplierAccesProccessApplication.class, args);
+            SpringApplication.run(SupplierAccessProccessApplication.class, args);
     }
 
 }
